085f9d2d0f9c8297393be89176fbdb62d118c74c
Joao <joaomarcio.ribeiro@gmail.com>
2021-04-23 14:00:08 -0300

    Update Palindrome.py

--- Palindrome.py
+++ Palindrome.py
@@ -1,13 +1,10 @@
 # Script to check if word is a palindrome
 
 def checkPalindrome(txt):
-
 	inverted = txt[::-1]
 	if txt == inverted:
 	   	print(f'{txt} is a palindrome!')
 	else:
 	   print(f'{txt} is not a palindrome!')
-    
-
 txt = str(input('Type a word and I will check if it is a palindrome: ')).lower()
 checkPalindrome(txt)
